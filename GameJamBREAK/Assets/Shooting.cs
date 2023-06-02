fcfcc4a897f428038b5cdf560e573b210252b2ca
Andreas.R <anzz1232@hotmail.com>
2023-06-02 18:52:57 +0200

    MAterial

---
+++ GameJamBREAK/Assets/Shooting.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bulletSpawnLocation;
+    [SerializeField] private int ammunition;
+     
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
