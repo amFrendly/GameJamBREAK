9597b3e007afbe55000272230522701e2aace3ff
Johannes Widén <johanneswiden2003@gmail.com>
2023-06-02 18:41:07 +0200

    Awesome movement

---
+++ GameJamBREAK/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Camera cam;
+
+    [SerializeField]
+    float jumpForce;
+
+    bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        LookAround();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    [SerializeField]
+    float walkingSpeed;
+
+    private void FixedUpdate()
+    {
+        //Constantly walking at walkingSpeed!
+        float deltaSpeed = walkingSpeed - rb.velocity.magnitude;
+        if (deltaSpeed > 0)rb.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z).normalized * deltaSpeed, ForceMode.VelocityChange);
+    }
+
+    [SerializeField]
+    float lookSpeed, lookXLimit;
+
+    float rotationX;
+
+    private void LookAround()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
+    }
+}
