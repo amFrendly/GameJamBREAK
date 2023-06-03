d5f19d028a18fe10d0698b3a5e628ad017bd8658
Johannes Widén <johanneswiden2003@gmail.com>
2023-06-03 17:18:33 +0200

    level 2 and kill script and better floor color :P

---
+++ GameJamBREAK/Assets/Scripts/KillScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class KillScript : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask killLayer;
+
+    public void Kill()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((killLayer & (1 << collision.gameObject.layer)) != 0)
+        {
+            Kill();
+        }
+    }
+}
