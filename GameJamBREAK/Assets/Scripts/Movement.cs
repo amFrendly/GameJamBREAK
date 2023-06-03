bfbd318b2a1681eb0dfc839967afbaae70789705
Jack Forsberg <an5190@mau.se>
2023-06-03 14:42:05 +0200

    Models

--- GameJamBREAK/Assets/Scripts/Movement.cs
+++ GameJamBREAK/Assets/Scripts/Movement.cs
@@ -1,378 +1,82 @@
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
-/// <summary>
-/// Q3-based first person controller
-/// </summary>
 public class Movement : MonoBehaviour
 {
-    #region Drag Drop
-    [Header("Components")]
-    [SerializeField]
-    private Transform _camTransform = default;
-
-    // Collision resolving is done with respect to this volume
-    [SerializeField]
-    private CapsuleCollider _collisionVolume = default;
-
-    // Collision will not happend with these layers
-    // One of them has to be this controller's own layer
-    [SerializeField]
-    private LayerMask _excludedLayers = default;
+    Rigidbody rb;
+    Camera cam;
 
     [SerializeField]
-    private bool _debugInfo = default;
+    float jumpForce;
 
-    [SerializeField]
-    private List<Transform> _groundedRayPositions = default;
-    #endregion
+    bool isGrounded;
+    bool hasJumped;
+    bool jump;
 
-    #region Movement Parameters
-    [Header("Movement parameters")]
-    // The controller can collide with colliders within this radius
-    [SerializeField]
-    private float _radius = 2f;
-
-    // Ad-hoc approach to make the controller accelerate faster
-    [SerializeField]
-    private float _groundAccelerationCoeff = 500.0f;
-
-    // How fast the controller accelerates while it's not grounded
-    [SerializeField]
-    private float _airAccelCoeff = 1f;
-
-    // Air deceleration occurs when the player gives an input that's not aligned with the current velocity
-    [SerializeField]
-    private float _airDecelCoeff = 1.5f;
-
-    // Along a dimension, we can't go faster than this
-    // This dimension is relative to the controller, not global
-    // Meaning that "max speend along X" means "max speed along 'right side' of the controller"
-    [SerializeField]
-    private float _maxSpeedAlongOneDimension = 8f;
-
-    // How fast the controller decelerates on the grounded
-    [SerializeField]
-    private float _friction = 15;
-
-    // Stop if under this speed
-    [SerializeField]
-    private float _frictionSpeedThreshold = 0.5f;
-
-    // Push force given when jumping
-    [SerializeField]
-    private float _jumpStrength = 8f;
-
-    // yeah...
-    [SerializeField]
-    private float _gravityAmount = 24f;
-
-    // How precise the controller can change direction while not grounded 
-    [SerializeField]
-    private float _airControlPrecision = 16f;
-
-    // When moving only forward, increase air control dramatically
-    [SerializeField]
-    private float _airControlAdditionForward = 8f;
-    #endregion
-
-    #region Fields
-    // The real velocity of this controller
-    private Vector3 _velocity;
-
-    // Raw input taken with GetAxisRaw()
-    private Vector3 _moveInput;
-
-    // Vertical look
-    private float _pitch = 0; // We keep track of this value since we want to clamp it
-    private const float Sensitivity = 150;
-
-    // Caching...
-    private readonly Collider[] _overlappingColliders = new Collider[10]; // Hope no more is needed
-    private Transform _ghostJumpRayPosition;
-
-    // Some information to persist
-    private bool _isGroundedInPrevFrame;
-    private bool _isGonnaJump;
-    private Vector3 _wishDirDebug;
-    #endregion
-
-    private void Start()
+    void Start()
     {
-        _ghostJumpRayPosition = _groundedRayPositions[_groundedRayPositions.Count - 1];
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-
-
-    private void Update()
+    void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked; // Keep doing this. We don't want cursor anywhere just yet
+        LookAround();
 
-        float dt = Time.deltaTime;
-
-        // We use GetAxisRaw, since we need it to feel as responsive as possible
-        _moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-        if (Input.GetKeyDown(KeyCode.Space) && !_isGonnaJump)
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded) rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.Space) && !hasJumped && isGrounded)
         {
-            _isGonnaJump = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _isGonnaJump = false;
-        }
-
-        // Mouse look
-        _pitch += Input.GetAxis("Mouse Y") * -Sensitivity * dt;
-        _pitch = Mathf.Clamp(_pitch, -89, 89);
-        _camTransform.localRotation = Quaternion.Euler(Vector3.right * _pitch);
-        transform.rotation *= Quaternion.Euler(Input.GetAxis("Mouse X") * Sensitivity * dt * Vector3.up);
-
-        // Reset player -- makes testing much easier
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            transform.position = new Vector3(0, 5, 0);
-            _velocity = Vector3.forward;
-        }
-
-        // MOVEMENT
-        Vector3 wishDir = _camTransform.TransformDirectionHorizontal(_moveInput); // We want to go in this direction
-        _wishDirDebug = wishDir.ToHorizontal();
-
-        bool isGrounded = IsGrounded(out Vector3 groundNormal);
-
-
-        if (isGrounded) // Ground move
-        {
-            // Don't apply friction if just landed or about to jump
-            if (_isGroundedInPrevFrame && !_isGonnaJump)
-            {
-                ApplyFriction(ref _velocity, dt);
-            }
-
-            Accelerate(ref _velocity, wishDir, _groundAccelerationCoeff, dt);
-
-            // Crop up horizontal velocity component
-            _velocity = Vector3.ProjectOnPlane(_velocity, groundNormal);
-            if (_isGonnaJump)
-            {
-                // Jump away
-                _velocity += Vector3.up * _jumpStrength;
-            }
-        }
-        else // Air move
-        {
-            // If the input doesn't have the same facing with the current velocity
-            // then slow down instead of speeding up
-            float coeff = Vector3.Dot(_velocity, wishDir) > 0 ? _airAccelCoeff : _airDecelCoeff;
-
-            Accelerate(ref _velocity, wishDir, coeff, dt);
-
-            if (Mathf.Abs(_moveInput.z) > 0.0001) // Pure side velocity doesn't allow air control
-            {
-                ApplyAirControl(ref _velocity, wishDir, dt);
-            }
-
-            _velocity += Vector3.down * (_gravityAmount * dt);
-        }
-
-        Vector3 displacement = _velocity * dt;
-
-        // If we're moving too fast, make sure we don't hollow through any collider
-        if (displacement.magnitude > _collisionVolume.radius)
-        {
-            ClampDisplacement(ref _velocity, ref displacement, transform.position);
-        }
-
-        transform.position += displacement;
-
-        Vector3 collisionDisplacement = ResolveCollisions(ref _velocity);
-
-        transform.position += collisionDisplacement;
-        _isGroundedInPrevFrame = isGrounded;
-
-        // Testing
-        //if (Input.GetKeyDown(KeyCode.G))
-        //{
-        //    Gravity.Set(Vector3.right);
-        //    _transform.rotation = Quaternion.LookRotation(Gravity.Forward, -Gravity.Down);
-        //}
-    }
-
-    private void Accelerate(ref Vector3 playerVelocity, Vector3 accelDir, float accelCoeff, float dt)
-    {
-        // How much speed we already have in the direction we want to speed up
-        float projSpeed = Vector3.Dot(playerVelocity, accelDir);
-
-        // How much speed we need to add (in that direction) to reach max speed
-        float addSpeed = _maxSpeedAlongOneDimension - projSpeed;
-        if (addSpeed <= 0)
-        {
-            return;
-        }
-
-        // How much we are gonna increase our speed
-        // maxSpeed * dt => the real deal. a = v / t
-        // accelCoeff => ad hoc approach to make it feel better
-        float accelAmount = accelCoeff * _maxSpeedAlongOneDimension * dt;
-
-        // If we are accelerating more than in a way that we exceed maxSpeedInOneDimension, crop it to max
-        if (accelAmount > addSpeed)
-        {
-            accelAmount = addSpeed;
-        }
-
-        playerVelocity += accelDir * accelAmount; // Magic happens here
-    }
-
-    private void ApplyFriction(ref Vector3 playerVelocity, float dt)
-    {
-        float speed = playerVelocity.magnitude;
-        if (speed <= 0.00001f)
-        {
-            return;
-        }
-
-        float downLimit = Mathf.Max(speed, _frictionSpeedThreshold); // Don't drop below treshold
-        float dropAmount = speed - (downLimit * _friction * dt);
-        if (dropAmount < 0)
-        {
-            dropAmount = 0;
-        }
-
-        playerVelocity *= dropAmount / speed; // Reduce the velocity by a certain percent
-    }
-
-    private void ApplyAirControl(ref Vector3 playerVelocity, Vector3 accelDir, float dt)
-    {
-        // This only happens in the horizontal plane
-        // TODO: Verify that these work with various gravity values
-        Vector3 playerDirHorz = playerVelocity.ToHorizontal().normalized;
-        float playerSpeedHorz = playerVelocity.ToHorizontal().magnitude;
-
-        float dot = Vector3.Dot(playerDirHorz, accelDir);
-        if (dot > 0)
-        {
-            float k = _airControlPrecision * dot * dot * dt;
-
-            // CPMA thingy:
-            // If we want pure forward movement, we have much more air control
-            bool isPureForward = Mathf.Abs(_moveInput.x) < 0.0001f && Mathf.Abs(_moveInput.z) > 0;
-            if (isPureForward)
-            {
-                k *= _airControlAdditionForward;
-            }
-
-            // A little bit closer to accelDir
-            playerDirHorz = playerDirHorz * playerSpeedHorz + accelDir * k;
-            playerDirHorz.Normalize();
-
-            // Assign new direction, without touching the vertical speed
-            playerVelocity = (playerDirHorz * playerSpeedHorz).ToHorizontal() + Vector3.up * playerVelocity.y;
-        }
-
-    }
-
-    // Calculates the displacement required in order not to be in a world collider
-    private Vector3 ResolveCollisions(ref Vector3 playerVelocity)
-    {
-        // Get nearby colliders
-        Physics.OverlapSphereNonAlloc(transform.position, _radius + 0.1f,
-            _overlappingColliders, ~_excludedLayers);
-
-        Vector3 totalDisplacement = Vector3.zero;
-        HashSet<int> checkedColliderIndices = new();
-
-        // If the player is intersecting with that environment collider, separate them
-        for (int i = 0; i < _overlappingColliders.Length; i++)
-        {
-            // Two player colliders shouldn't resolve collision with the same environment collider
-            if (checkedColliderIndices.Contains(i))
-            {
-                continue;
-            }
-
-            Collider envColl = _overlappingColliders[i];
-
-            // Skip empty slots
-            if (envColl == null)
-            {
-                continue;
-            }
-
-            if (Physics.ComputePenetration(
-                _collisionVolume, _collisionVolume.transform.position, _collisionVolume.transform.rotation,
-                envColl, envColl.transform.position, envColl.transform.rotation,
-                out Vector3 collisionNormal, out float collisionDistance))
-            {
-                // Ignore very small penetrations
-                // Required for standing still on slopes
-                // ... still far from perfect though
-                if (collisionDistance < 0.015f)
-                {
-                    continue;
-                }
-
-                checkedColliderIndices.Add(i);
-
-                // Get outta that collider!
-                totalDisplacement += collisionNormal * collisionDistance;
-
-                // Crop down the velocity component which is in the direction of penetration
-                playerVelocity -= Vector3.Project(playerVelocity, collisionNormal);
-            }
-        }
-
-        // It's better to be in a clean state in the next resolve call
-        for (int i = 0; i < _overlappingColliders.Length; i++)
-        {
-            _overlappingColliders[i] = null;
-        }
-
-        return totalDisplacement;
-    }
-
-    // If one of the rays hit, we're considered to be grounded
-    private bool IsGrounded(out Vector3 groundNormal)
-    {
-        groundNormal = Vector3.up;
-
-        bool isGrounded = false;
-        foreach (Transform t in _groundedRayPositions)
-        {
-            // The last one is reserved for ghost jumps
-            // Don't check that one if already on the ground
-            if (t == _ghostJumpRayPosition && isGrounded)
-            {
-                continue;
-            }
-
-            if (Physics.Raycast(t.position, Vector3.down, out RaycastHit hit, 0.51f, ~_excludedLayers))
-            {
-                groundNormal = hit.normal;
-                isGrounded = true;
-            }
-        }
-
-        return isGrounded;
-    }
-
-    // If there's something between the current position and the next, clamp displacement
-    private void ClampDisplacement(ref Vector3 playerVelocity, ref Vector3 displacement, Vector3 playerPosition)
-    {
-        if (Physics.Raycast(playerPosition, playerVelocity.normalized, out RaycastHit hit, displacement.magnitude, ~_excludedLayers))
-        {
-            displacement = hit.point - playerPosition;
+            jump = true;
+            hasJumped = true;
         }
     }
 
-    // Handy when testing
-    public void ResetAt(Transform t)
+    [SerializeField]
+    float walkingSpeed;
+
+    private void FixedUpdate()
     {
-        transform.position = t.position + Vector3.up * 0.5f;
-        _camTransform.position = transform.position;
-        _velocity = t.TransformDirection(Vector3.forward);
+        //Constantly walking at walkingSpeed!
+        //float deltaSpeed = walkingSpeed - rb.velocity.magnitude;
+        // if (deltaSpeed > 0)rb.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z).normalized * deltaSpeed, ForceMode.VelocityChange);
+        if (isGrounded)
+        {
+            rb.AddForce(-new Vector3(rb.velocity.x, 0, rb.velocity.z) * .5f, ForceMode.VelocityChange);
+        }
+
+        if (jump)
+        {
+            rb.AddForce(new Vector3(transform.forward.x, 1, transform.forward.z) * 10, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+
+    [SerializeField]
+    float lookSpeed, lookXLimit;
+
+    float rotationX;
+
+    private void LookAround()
+    {
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
+        hasJumped = false;
     }
 
 }
